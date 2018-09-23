36fd0dad1f2331b708518c492f69999e5566708b
Philip Kalinda <philipkalinda@gmail.com>
2018-09-23 22:13:06 +0100

    Create stackers.py

---
+++ stackerpy/stackers.py
@@ -0,0 +1,41 @@
+import numpy as np
+import pandas as pd
+
+class ModelStacker():
+	"""
+	Model Stacker
+	"""
+	
+	def __init__(self, models, stacker, blending=False):
+		self.models = models
+		self.stacker = stacker
+		self.best_models = {}
+		self.best_stacker = {}
+		self.blending = blending
+	
+	def fit(X, Y):
+		raise NotImplementedError()
+	
+	def predict(X):
+		raise NotImplementedError()
+		
+
+class ExplorativeModelStacker():
+	"""
+	Explorative Model Stacker
+	"""
+	
+	def __init__(self, n_models, blending=False, optimize=False):
+		self.n_models = n_models
+		self.stacker = None
+		self.best_models = {}
+		self.best_stacker = {}
+		self.blending = blending
+		self.optimize = optimize
+	
+	def fit(X, Y):
+		raise NotImplementedError()
+	
+	def predict(X):
+		raise NotImplementedError()
+		
