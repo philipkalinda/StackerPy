e9e33d982cb9f6d0b3f80422c6c294e6055648db
Philip Kalinda <philipkalinda@gmail.com>
2019-09-26 16:42:09 +0100

    Update test.py

--- stackerpy/test.py
+++ stackerpy/test.py
@@ -24,7 +24,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 
 
 # ----------------------------------------
-# Original ridge classifier results / eval
+# Original classifier results / eval
 # ----------------------------------------
 
 ridge = RandomForestClassifier()
